cd3a2df07d3d69a1dd7ef177eee4dedd73fa844a
Arun Mozhi <Arul#fouvi.com>
2023-11-21 12:56:24 +0530

    server.js

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
-//Route Files
 
-const bootcamp = require('./routes/bootcamp')
+// Route Files
+const bootcamp = require('./routes/bootcamp');
 
-//load env 
-dotenv.config({ path: './config/config.env'});
+// Load env
+dotenv.config({ path: './config/config.env' });
 
-//mount router
-app.get('/', ( req, res) =>{
- res.send ("<h1>hello team</h1>")
-});
+const connectDB = require ('./config/db')
+
+// Connect to the database
+connectDB();
+
+// Dev log middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount router
+app.use('/api/v1/bootcamps', bootcamp);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+});
+
+
+//handle unhandle promise rejection
+process.on ('unhandleRejection',(err,promise)=>{
+  console.log(`Error: ${err.message}`);
+  server.close(()=>process.exit(1)
+   )
 });
